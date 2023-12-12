a87bb12d911d457bdb270565e7258e81f57bd684
Thinh Dang <thinh191204033@gmail.com>
2023-12-12 10:42:38 +0700

    Add project files.

---
+++ DecarloStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DecarloStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
